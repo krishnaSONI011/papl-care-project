2c866acbb25276a596569fe3783d8ea5a13612f8
krishna soni <soni01krishna@gmail.com>
2023-09-21 16:36:18 +0530

    converting csv data into usefull json for point 2

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express =require('express')
 const axios = require('axios')
+const csv = require('csv-parser');
+const fs = require('fs')
 const cors =require('cors')
 let app = express()
 
@@ -18,3 +20,35 @@ app.get("/",async (req,res)=>{
         console.error(err)
     }
 })
+
+app.post('/filterData', (req, res) => {
+    const { vehicleNo, startDate, endDate } = req.body;
+  
+    if (!vehicleNo || !startDate || !endDate) {
+      return res.status(400).json({ error: 'Missing parameters: vehicleNo, startDate, or endDate' });
+    }
+  
+    const results = [];
+    const csvFilePath = 'gps.csv';
+    fs.createReadStream(csvFilePath)
+      .pipe(csv())
+      .on('data', (row) => {
+        if (row.VehicleNo === vehicleNo) {
+          const rowDate = new Date(row.Date);
+          const startDateTime = new Date(startDate);
+          const endDateTime = new Date(endDate);
+  
+          if (rowDate >= startDateTime && rowDate <= endDateTime) {
+            results.push(row);
+          }
+        }
+      })
+      .on('end', () => {
+        console.log('CSV file reading and filtering complete.');
+        res.json({ data: results });
+      })
+      .on('error', (error) => {
+        console.error('Error reading CSV file:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+      });
+  });
